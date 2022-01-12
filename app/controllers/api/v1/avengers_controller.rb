0f8c9e5278848e9353d2ae36a5b534d615beb468
alejandro calzadilla <alejandrocalzadilla@gmail.com>
2022-01-12 12:09:09 -0500

    added route post added create method in avengers controller added avengers params

--- app/controllers/api/v1/avengers_controller.rb
+++ app/controllers/api/v1/avengers_controller.rb
@@ -7,12 +7,34 @@ class Api::V1::AvengersController < ActionController::API
   
     def show
       return render plain: 'avenger Not Found, Please try a different one', status: 404 unless (@avenger = set_avenger)
-      
     end
+
+    def create
+      new_avenger = Avenger.create(avengers_params)
+      if new_avenger.save
+        status = 'new avenger #{new_avenger.superhero_name} created'
+      else
+        render plain: 'sorry, avenger not created'
+
+      end
+
+      render plain: status
+    end
+  
 
     private
   
     def set_avenger
       @avenger = Avenger.find_by(id: params[:id])
+    end
+
+    def avengers_params
+      params.permit([
+        :superhero_name,
+        :real_name,
+        :age,
+        :description,
+        :status
+      ])
     end
   end
